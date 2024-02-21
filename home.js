77413efb5bce8edc05cf313a29b7efb16919082b
aggicreative555 <aggi.creative@gmail.com>
2024-02-21 15:57:59 +0100

    refractor(script): Add html elements, append items

--- home.js
+++ home.js
@@ -2,13 +2,41 @@ import { gamesApiUrl } from "./scripts/const.mjs";
 import { doFetch } from "./scripts/utils/doFetch.mjs";
 
 
+{/* <div class="product-wrapper">
+  <div class="product-container">
+    <div class="game-image"></div>
+    <div class="product-content">
+      <h3 class="product-title"></h3>
+      <h4 class="product-tags"></h4>
+      <div class="product-price">
+        <h4 class="sale"></h4>
+        <h4 class="original"></h4>
+      </div>
+    </div>
+  </div>
+</div> */}
+
 function generateGameHtml(game) {
+    const gameWrapper = document.createElement('div');
+    gameWrapper.classList.add('product-wrapper');
+
     const gameContainer = document.createElement('div');
     gameContainer.classList.add('product-container');
+
+    const gameImage = document.createElement('div');
+    gameImage.classList.add('game-image');
+    gameImage.style.backgroundImage = game.image.url;
+    
+    const gameContent = document.createElement('div');
+    gameContent.classList.add('product-content');
 
     const gameTitle = document.createElement('h3');
     gameTitle.classList.add('product-title');
     gameTitle.textContent = game.title;
+
+    const gameTags = document.createElement('h4');
+    gameTags.classList.add('product-tags');
+    gameTags.textContent = game.tags;
 
     const gamePriceContainer = document.createElement('div');
     gamePriceContainer.classList.add('product-price');
@@ -21,9 +49,12 @@ function generateGameHtml(game) {
     gameDiscountedPrice.classList.add('sale');
     gameDiscountedPrice.textContent = game.discountedPrice;
     
-    gameContainer.append(gameTitle, gamePrice, gameDiscountedPrice);
+    gameWrapper.appendChild(gameContainer);
+    gameContainer.append(gameImage, gameContent);
+    gameContent.append(gameTitle, gameTags, gamePriceContainer);
+    gamePriceContainer.append(gamePrice, gameDiscountedPrice);
 
-    return gameContainer;
+    return gameWrapper;
 }
 
 function displayGames(games) {
